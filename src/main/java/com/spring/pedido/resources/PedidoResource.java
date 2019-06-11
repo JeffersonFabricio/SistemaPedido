3b0b7d968765133928e8416fc4edfc30624cd58e
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-11 13:33:17 -0300

    EndPoint Paginação da Categoria

--- src/main/java/com/spring/pedido/resources/PedidoResource.java
+++ src/main/java/com/spring/pedido/resources/PedidoResource.java
@@ -13,16 +13,14 @@ import com.spring.pedido.services.PedidoService;
 @RestController
 @RequestMapping(value = "/pedidos")
 public class PedidoResource {
-	
+
 	@Autowired
 	private PedidoService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
-		
 		Pedido obj = service.find(id);
-		
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 }
--- src/main/java/com/spring/pedido/services/CategoriaService.java
+++ src/main/java/com/spring/pedido/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.spring.pedido.domain.Categoria;
@@ -21,32 +24,37 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return rep.findAll();
 	}
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = rep.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return rep.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return rep.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
-		
+
 		try {
 			rep.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possuí Produtos.");
 		}
-		
+
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return rep.findAll(pageRequest);
 	}
 
 }
