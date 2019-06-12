434ce9f74808f800c7f63ff24d50ea617529e159
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-12 15:05:39 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/spring/pedido/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.spring.pedido.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	public static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String msg) {
+		errors.add(new FieldMessage(fieldName, msg));
+	}	
+
+}
