629a059e8b6dbce77bedfba377c5bd37e684c46f
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-14 10:49:04 -0300

    Validação do E-mail no cliente

--- src/main/java/com/spring/pedido/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/spring/pedido/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.pedido.domain.Cliente;
 import com.spring.pedido.domain.enums.TipoCliente;
 import com.spring.pedido.dto.ClienteNewDTO;
+import com.spring.pedido.repositories.ClienteRepository;
 import com.spring.pedido.resources.exceptions.FieldMessage;
 import com.spring.pedido.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository rep;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()))
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		
+		Cliente aux = rep.findByEmail(objDto.getEmail());
+		if (aux != null)
+			list.add(new FieldMessage("email", "E-mail já cadastrado"));
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
