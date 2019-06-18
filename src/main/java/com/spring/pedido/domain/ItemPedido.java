3f35f2b970386f55c6209d7c4025a67cbce2b08d
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-18 10:55:57 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/spring/pedido/domain/ItemPedido.java
+++ src/main/java/com/spring/pedido/domain/ItemPedido.java
@@ -30,6 +30,10 @@ public class ItemPedido implements Serializable {
 		this.qtd = qtd;
 		this.preco = preco;
 	}
+	
+	public double getSubTotal() {
+		return ((preco - desconto) * qtd);
+	}
 
 	@JsonIgnore
 	public Pedido getPedido() {
